ba6986ed1c44cef961788cd0129bd9e79a9a6222
Alex Lockhart <salexlockhart@gmail.com>
2018-10-17 16:47:33 +0100

    Test fixes

--- test.js
+++ test.js
@@ -1,70 +1,45 @@
 import test from 'ava';
 import alfyTest from 'alfy-test';
-import moment from 'moment-timezone';
+import moment from 'moment';
 
-test.before(() => {
-	moment.tz.setDefault('Europe/London');
-});
+const date = moment(new Date(new Date(0).setUTCSeconds(1539784980)));
+const dateString = date.format('LLL');
+const seconds = String(date.unix());
+const milliseconds = String(date.valueOf());
+
+const expected = [
+	{
+		title: dateString,
+		subtitle: 'Date',
+		arg: dateString
+	}, {
+		title: seconds,
+		subtitle: 'Timestamp (s)',
+		arg: seconds
+	}, {
+		title: milliseconds,
+		subtitle: 'Timestamp (ms)',
+		arg: milliseconds
+	}
+];
 
 test('should convert given UTC timestamp in seconds', async t => {
 	const alfy = alfyTest();
 	const result = await alfy('1539784980');
 
-	t.deepEqual(result, [
-		{
-			title: 'October 17, 2018 3:03 PM',
-			subtitle: 'Date',
-			arg: 'October 17, 2018 3:03 PM'
-		}, {
-			title: '1539784980',
-			subtitle: 'Timestamp (s)',
-			arg: '1539784980'
-		}, {
-			title: '1539784980000',
-			subtitle: 'Timestamp (ms)',
-			arg: '1539784980000'
-		}
-	]);
+	t.deepEqual(result, expected);
 });
 
 test('should convert given US-style date string', async t => {
 	const alfy = alfyTest();
-	const result = await alfy('Oct 17 2018 3:03 PM GMT+1');
+	const result = await alfy('Oct 17 2018 2:03 PM UTC');
 
-	t.deepEqual(result, [
-		{
-			title: 'October 17, 2018 3:03 PM',
-			subtitle: 'Date',
-			arg: 'October 17, 2018 3:03 PM'
-		}, {
-			title: '1539784980',
-			subtitle: 'Timestamp (s)',
-			arg: '1539784980'
-		}, {
-			title: '1539784980000',
-			subtitle: 'Timestamp (ms)',
-			arg: '1539784980000'
-		}
-	]);
+	t.deepEqual(result, expected);
 });
 
 test('should convert given UK-style date string', async t => {
 	const alfy = alfyTest();
-	const result = await alfy('17 Oct 2018 3:03 PM GMT+1');
+	const result = await alfy('17 Oct 2018 2:03 PM UTC');
 
-	t.deepEqual(result, [
-		{
-			title: 'October 17, 2018 3:03 PM',
-			subtitle: 'Date',
-			arg: 'October 17, 2018 3:03 PM'
-		}, {
-			title: '1539784980',
-			subtitle: 'Timestamp (s)',
-			arg: '1539784980'
-		}, {
-			title: '1539784980000',
-			subtitle: 'Timestamp (ms)',
-			arg: '1539784980000'
-		}
-	]);
+	t.deepEqual(result, expected);
 });
